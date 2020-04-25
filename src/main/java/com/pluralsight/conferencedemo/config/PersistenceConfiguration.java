9241155f5790dca9fa86d7d6cb89e5135141cd54
sidshirore <63847014+sidshirore@users.noreply.github.com>
2020-04-25 19:59:45 +0530

    Commented out PersistenceConfiguration Bean again

--- src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
 
-@Configuration
+
 public class PersistenceConfiguration {
 //    @Bean
 //    public DataSource dataSource(){
