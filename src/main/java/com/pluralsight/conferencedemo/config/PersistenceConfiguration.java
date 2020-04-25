9fc9eccced8c40bc2cc135fef23e19beac123963
sidshirore <63847014+sidshirore@users.noreply.github.com>
2020-04-25 19:26:23 +0530

    First Commit

---
+++ src/main/java/com/pluralsight/conferencedemo/config/PersistenceConfiguration.java
@@ -0,0 +1,19 @@
+package com.pluralsight.conferencedemo.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfiguration {
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder builder = DataSourceBuilder.create();
+        builder.url("jdbc:postgresql://localhost:5432/conf-demo");
+        builder.username("postgres");
+        builder.password("root");
+        return builder.build();
+    }
+}
